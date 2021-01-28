17ce9a8ef2da1263b1f201cd6eb99ba2aaaf1e88
LucasGoe <75021271+LucasGoe@users.noreply.github.com>
2021-01-28 13:33:38 +0100

    removed solution

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-function getVolume(width, length, height) {
-    console.log("INPUT :", width, length, height);
-    return width * length * height;
+function getVolume() {
+    // WRITE ME!
+
 }
-const volume = getVolume(4, 5, 7);
-    console.log(volume);
+
 
 
 // 1. exporteren
